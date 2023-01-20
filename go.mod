976a1c68ceec879cc45702c5f0612d9830482c41
Moch Lutfi <kapten.lufi@gmail.com>
2023-01-20 02:03:55 +0000

    feat: update templates and replace deprecated func

--- go.mod
+++ go.mod
@@ -1,10 +1,15 @@
 module github.com/lumochift/optgen
 
-go 1.13
+go 1.19
 
 require (
-	github.com/davecgh/go-spew v1.1.0
-	github.com/gogo/protobuf v1.3.1
 	github.com/google/go-cmp v0.4.0
 	github.com/stretchr/testify v1.4.0
+	golang.org/x/text v0.6.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- main.go
+++ main.go
@@ -7,23 +7,29 @@ import (
 	"go/parser"
 	"go/printer"
 	"go/token"
-	"io/ioutil"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
 	"text/template"
 
 	"github.com/lumochift/optgen/generator"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
+func titleCase(s string) string {
+	return cases.Title(language.Und).String(s)
+}
+
 var (
-	sourceFile, tagName, structName, optName string
-	writeMode, allFields, showVersion        bool
+	sourceFile, tagName, structName   string
+	writeMode, allFields, showVersion bool
 
 	version string = "dev"
 
 	funcMap = template.FuncMap{
-		"title":   strings.Title,
+		"title":   titleCase,
 		"toLower": strings.ToLower,
 	}
 )
@@ -32,7 +38,6 @@ func initCLI() {
 	flag.StringVar(&sourceFile, "file", "", "path file")
 	flag.StringVar(&tagName, "tag", "opt", "custom tag")
 	flag.StringVar(&structName, "name", "", "struct name")
-	flag.StringVar(&optName, "optname", "Option", "option name")
 	flag.BoolVar(&writeMode, "w", false, "enable write mode")
 	flag.BoolVar(&showVersion, "v", false, "show version")
 	flag.BoolVar(&allFields, "all", false, "generate all fields")
@@ -53,10 +58,11 @@ func main() {
 		log.Fatalf("Source file and struct name must be provided.\n%s", exampleMessage)
 	}
 
-	source, err := ioutil.ReadFile(sourceFile)
+	source, err := os.ReadFile(sourceFile)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	fset := token.NewFileSet()
 	f, err := parser.ParseFile(fset, "", source, 0)
 
@@ -67,7 +73,6 @@ func main() {
 	type TemplateData struct {
 		StructName string
 		Tags       []generator.Tag
-		OptName    string
 	}
 
 	if allFields {
@@ -78,13 +83,12 @@ func main() {
 
 	tmpl := template.Must(template.New("generator").Funcs(funcMap).Parse(generator.CodeTemplate))
 	var buf bytes.Buffer
-
+	fmt.Println(tags, structName)
 	if err := tmpl.Execute(&buf, TemplateData{
 		Tags:       tags,
 		StructName: structName,
-		OptName:    optName,
 	}); err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 
 	if !writeMode {
@@ -93,7 +97,7 @@ func main() {
 	}
 	var sourceBuf bytes.Buffer
 	printer.Fprint(&sourceBuf, fset, f)
-	if err := ioutil.WriteFile(sourceFile, append(sourceBuf.Bytes(), buf.Bytes()...), 0644); err != nil {
+	if err := os.WriteFile(sourceFile, append(sourceBuf.Bytes(), buf.Bytes()...), 0644); err != nil {
 		log.Println(err)
 	}
 	// format generated code
